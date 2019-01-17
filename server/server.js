67a2c8634ec2a649ce347b9d65cd55f68903725d
daniel nava <dnavamoslercl@gmail.com>
2019-01-17 16:00:02 -0300

    conexion Activo activo

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@
 /*================================== */
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io')
+const http = require('http')
 
 /*================================== /*
       Instancia de express
 /*================================== */
 const app = express();
+
+// definir servidor http
+let server = http.createServer(app)
 
 /*================================== /*
   declarar path para carpeta publica
@@ -22,11 +27,45 @@ const port = process.env.PORT || 3000;
  /*================================== */
 app.use(express.static(publicPath));
 
+// Inicializar socket
+let io = socketIO(server)
+
+io.on('connection', (client) => {
+    console.log(`se conecto al servidor.`)
+
+
+    // Enviar mensaje al cliente
+
+    client.emit('escucharMensaje',{
+        usuario : 'administrador',
+        message : 'Bienvenido usuario'
+
+    })
+
+    // reportar una desconexion
+    client.on('disconnect' ,() => {
+        console.log(`Un usuario se ha desconectado`)
+    })
+
+    // Escuchar mensaje del cliente
+    client.on('enviarMessage',(message, cb) => {
+        if(message.nombre){
+            cb({
+                resp : `Todo salio bien!`
+            })
+        }
+        else{
+            cb({
+                resp : `TODO SALIO MAL!!!!`
+            })
+        }
+    })
+
+} )
 /*================================== /*
         Servidor Corriendo
 /*================================== */
-app.listen(port, (err) => {
-sda
+server.listen(port, (err) => {
     if (err) throw new Error(err);
 
     console.log(`Servidor corriendo en puerto ${ port }`);
